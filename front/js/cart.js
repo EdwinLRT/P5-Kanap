346eecaac9241880a3e3d785a8595bbc7cceb2c1
Edwin Laurent <laurent.edwin@icloud.com>
2022-08-30 16:17:32 +0200

    update

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,9 +1,53 @@
+//Call API to get product list
+fetch("http://localhost:3000/api/products")
+    // if result is ok - turn into Json
+    .then(function(res){
+        if (res.ok){
+            return res.json();
+    }})
+
 //Array to display LocalStorage 
 const cart =[]
+let productInLocalStorage = JSON.parse(localStorage.getItem("product"));
+
+//Empty cart
+if (productInLocalStorage == null || productInLocalStorage.length == 0) {
+
+    document.querySelector("h1").innerHTML = `Votre panier est vide`
+    
+}
+
 
 //load LocalStorage in cart
 function getItemsFromLocalStorage(params) {
-    let items = localStorage.length
-    console.log(items)
+    for (let i = 0; i < localStorage.length; i++){
+        let key = localStorage.key(i);
+        let value = localStorage.getItem(key);
+        console.log(key, value);
+        let createProductInCart = document.querySelector('#cart__items')
+            createProductInCart.innerHTML = `<article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
+            <div class="cart__item__img">
+            <img src="../images/product01.jpg" alt="Photographie d'un canapé">
+            </div>
+            <div class="cart__item__content">
+            <div class="cart__item__content__description">
+                <h2>Nom du produit</h2>
+                <p>Vert</p>
+                <p>42,00 €</p>
+            </div>
+            <div class="cart__item__content__settings">
+                <div class="cart__item__content__settings__quantity">
+                <p>Qté : </p>
+                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+                </div>
+                <div class="cart__item__content__settings__delete">
+                <p class="deleteItem">Supprimer</p>
+                </div>
+            </div>
+            </div>
+            </article> 
+            `
+    }
 }
 getItemsFromLocalStorage()
+
