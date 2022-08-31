454056e0999fcdc26a6a71039f11788e9730bd6d
Edwin Laurent <laurent.edwin@icloud.com>
2022-08-31 16:27:19 +0200

    update

--- front/js/cart.js
+++ front/js/cart.js
@@ -8,52 +8,89 @@ fetch("http://localhost:3000/api/products")
 
 let productInLocalStorage = JSON.parse(localStorage.getItem("productOptions"));
 
+
+// -----------
+// Cart
+//------------
+
+
 //Empty cart
 if (productInLocalStorage == null || productInLocalStorage.length == 0) {
 
-    document.querySelector("h1").innerHTML = `Votre panier est vide`
+    document.querySelector("h1").innerHTML = `Votre panier est vide`;
     
 }
-//Price multiplicator for each item
 
-
-
+//creating array for cart totals
+let totalPriceArray = [];
+let totalQuantityArray = [];
 
 //load LocalStorage in cart
 
 for (i = 0 ; i < productInLocalStorage.length ; i += 1) {
     document.querySelector("#cart__items").innerHTML +=    
-     `<article class="cart__item" data-id="${productInLocalStorage[i].id}">
-        <article class="cart__item" data-id="${productInLocalStorage[i].id}">
-            <div class="cart__item__img">
-                 <img src="${productInLocalStorage[i].img}" alt="${productInLocalStorage[i].altTxt}">
-            </div>
-            <div class="cart__item__content">
-                <div class="cart__item__content__titlePrice">
-                     <h2>${productInLocalStorage[i].name}</h2>
-                     <p>${productInLocalStorage[i].price * productInLocalStorage[i].quantity}€</p>
-                </div>
-            <div class="cart__item__content__settings">
-                <div class="cart__item__content__settings__quantity">
-                    <p>Couleur : ${productInLocalStorage[i].color}</p>
-                    <p>Qté : </p>
-                    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" canapeId="${productInLocalStorage[i].id}" canapeColor="${productInLocalStorage[i].color}" value="${productInLocalStorage[i].quantity}">
-                </div>
-                <div class="cart__item__content__settings__delete">
-                     <p class="deleteItem" canapeId="${productInLocalStorage[i].id}" canapeColor="${productInLocalStorage[i].color}">Supprimer</p>
-                </div>
-            </div>
-            </div>
-        </article>
-    </article>`;
+  `<article class="cart__item" data-id="${productInLocalStorage[i].id}" data-color="${productInLocalStorage[i].color}">
+    <div class="cart__item__img">
+      <img src="${productInLocalStorage[i].img}" alt="${productInLocalStorage[i].altTxt}">
+    </div>
+    <div class="cart__item__content">
+      <div class="cart__item__content__description">
+        <h2>${productInLocalStorage[i].name}</h2>
+        <p>${productInLocalStorage[i].color}</p>
+        <p>${productInLocalStorage[i].price}€</p>
+      </div>
+      <div class="cart__item__content__settings">
+        <div class="cart__item__content__settings__quantity">
+          <p>Qté : </p>
+          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${productInLocalStorage[i].quantity}>
+        </div>
+        <div class="cart__item__content__settings__delete">
+          <p class="deleteItem" productId="${productInLocalStorage[i].id}" productColor="${productInLocalStorage[i].color}">Supprimer</p>
+        </div>
+      </div>
+    </div>
+  </article>`
 
     
-    let totalPrice = [];
-    let totalQuantity = [];
-    totalPrice.push(parseInt(productInLocalStorage[i].price));
-    totalQuantity.push(parseInt(productInLocalStorage[i].quantity));
-    console.log(totalPrice)
-    console.log(totalQuantity)
+    let totalPriceCalculation = parseInt(productInLocalStorage[i].price) * parseInt(productInLocalStorage[i].quantity)
+    totalPriceArray.push(totalPriceCalculation);
+    totalQuantityArray.push(parseInt(productInLocalStorage[i].quantity));
+    let totalPrice=0
+
+// Remove item from cart
+// let removeProduct = document.getElementsByClassName("deleteItem");
+//     removeProduct.addEventListener('click', e => 
+//     console.log("click")
+//     )
 
 }
 
+// Total Price
+let totalPrice=0
+for (let i = 0; i < totalPriceArray.length; i++) {
+    totalPrice += totalPriceArray[i];
+}
+document.querySelector("#totalPrice").textContent = totalPrice;
+
+// Total Quantity
+let totalQuantity=0
+for (let i = 0; i < totalQuantityArray.length; i++) {
+    totalQuantity += totalQuantityArray[i];
+}
+document.querySelector("#totalQuantity").textContent = totalQuantity;
+
+
+
+// Delete produt from cart
+
+
+
+
+
+// -----------
+// Form
+//------------
+
+// let regexLastName = /^[a-z ,.'-]{0,35}$/i
+
+// <input type="text" pattern="[0-9]{,3}" />
